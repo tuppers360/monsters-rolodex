eb6167f63a7fec6d6efc53f6176706d12e582ed3
tuppers360 <tuppers360@live.co.uk>
2019-10-08 19:55:37 +0100

    Updates

---
+++ precache-manifest.04b826cef85da703a5130b895877c821.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048b132832bdf1364e43c6a75518ab91",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0b5b0e3abd559f8c6e91",
+    "url": "/monsters-rolodex/static/css/main.53e7b94f.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "0b5b0e3abd559f8c6e91",
+    "url": "/monsters-rolodex/static/js/main.5cdffe47.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
